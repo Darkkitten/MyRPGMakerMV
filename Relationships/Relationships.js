b5cd2b5b2c52d4c10f677d159d4852555974a666
darkkitten <darkkitten@gmail.com>
2015-12-05 07:29:53 -0700

    Started to add base to the Script.

--- Relationships/Relationships.js
+++ Relationships/Relationships.js
@@ -113,49 +113,77 @@ Darkkitten.Parameters = PluginManager.parameters('Relationships');
 var Imported = Imported || {};
 Imported.Relationships = true;
 
-var Use_Custom_Background = Darkkitten.Parameters['Use_custom_Background'];
-var Background = Darkkitten.Parameters['Relationship Background Image'];
-var Window_Title = Darkkitten.Parameters['Window Title'];
-var Use_Menu_Command = Darkkitten.Parameters['Use Menu Command'];
-var Use_Leveling_System = Darkkitten.Parameters['Use Leveling System']
-Array Terms_Of_Feelings = Darkkitten.Parameters['Terms Of Feelings'];
-var Term_Of_Mastered = Darkkitten.Parameters['Term Of Mastered'];
-var Use_Polarities = Darkkitten.Parameters['Use Polarities'];
+function() {	
+	var Use_Custom_Background = Darkkitten.Parameters['Use_custom_Background'];
+	var Background = Darkkitten.Parameters['Relationship Background Image'];
+	var Window_Title = Darkkitten.Parameters['Window Title'];
+	var Use_Menu_Command = Darkkitten.Parameters['Use Menu Command'];
+	var Use_Leveling_System = Darkkitten.Parameters['Use Leveling System']
+	Array Terms_Of_Feelings = Darkkitten.Parameters['Terms Of Feelings'];
+	var Term_Of_Mastered = Darkkitten.Parameters['Term Of Mastered'];
+	var Use_Polarities = Darkkitten.Parameters['Use Polarities'];
 
-var Use_Level_Icons = Darkkitten.Parameters['Use Level Icons'];
-var Use_Title = Darkkitten.Parameters['Use Title'];
-var Use_Location = Darkkitten.Parameters['Use Location'];
-var Use_Liked_and_Disliked_Items = Darkkitten.Parameters['Use Liked and Disliked Items'];
-var Use_Feelings = Darkkitten.Parameters['Use Feelings'];
-var Use_Current_Status = Darkkitten.Parameters['Use Current Status'];
-var Use_Gauges = Darkkitten.Parameters['Use Gauges'];
+	var Use_Level_Icons = Darkkitten.Parameters['Use Level Icons'];
+	var Use_Title = Darkkitten.Parameters['Use Title'];
+	var Use_Location = Darkkitten.Parameters['Use Location'];
+	var Use_Liked_and_Disliked_Items = Darkkitten.Parameters['Use Liked and Disliked Items'];
+	var Use_Feelings = Darkkitten.Parameters['Use Feelings'];
+	var Use_Current_Status = Darkkitten.Parameters['Use Current Status'];
+	var Use_Gauges = Darkkitten.Parameters['Use Gauges'];
 
-var Use_Icons = Darkkitten.Parameters['Use Icons'];
+	var Use_Icons = Darkkitten.Parameters['Use Icons'];
 
-var getInformation_pluginCommand = Game_Interpreter.prototype.pluginCommand;
-Game_Interpreter.prototype.pluginCommand = function(command, args) {
-    getInformation_pluginCommand.call(this, command, args);
-    if (command === 'Relationships'){
-		switch (args[0].toLowercase()){
+	var getInformation_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+	Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    	getInformation_pluginCommand.call(this, command, args);
+   	 if (command === 'Relationships'){
+		 switch (args[0].toLowercase()){
 			case 'open':
 			break;
-			case 'add':
+			case 'add_relationship':
+			$gameSystem.add_relationship(args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12], args[13], args[14], args[15], args[16], args[17], args[18], args[19] ); //19
 			break;
 			case 'change_location':
+			$gameSystem.change_location(args[1], args[2]);
 			break;
 			case 'change_status':
+			$gameSystem.change_status(args[1], args[2]);
 			break;
 			case 'change_lvl_icons':
+			$gameSystem.change_lvl_icons(args[1], args[2], args[3], args[4], args[5]); //5
+			break;
+			case 'change_liked_item':
+			$gameSystem.change_liked_item(arg[1], arg[2]);
+			break;
+			case 'change_diskliked_item':
+			$gameSystem.change_disliked_item(arg[1], arg[2]);
 			break;
 			case 'change_lvl4_reward':
+			$gameSystem.change_lvl4_reward(arg[1], arg[2]);
 			break;
 			case 'change_lvl8_reward':
+			$gameSystem.change_lvl8_reward(arg[1], arg[2]);
 			break;
 			case 'change_lvl12_reward':
+			$gameSystem.change_lvl12_reward(arg[1], arg[2]);
 			break;
 		}
 	}
-    
-    
-    
-    
+	};
+   
+	Game_System.prototype.add_relationship = function(name, title, relationship_variable, relationship_level_variable, face_name, face_index, location, description1, description2, description3, description4, description5, description6, current_status, lv_4_gift, lv_8_gift, lv_12_gift, favorite_gift, worst_gift){
+		
+	};
+	
+	Game_System.prototype.change_location = function(name, location){
+		
+	};
+	
+	Game_System.prototype.change_status = function(name, location){
+		
+	};
+	
+	
+	
+	
+}();
